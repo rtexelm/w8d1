9fecd936d53718be779218bd0d5e0bb328b09841
Bonnie Li <bonnieli@Bonnies-MacBook-Pro.local>
2022-10-31 17:06:00 -0400

    Ran server Hello World

--- RailsLite/bin/p01_basic_server.rb
+++ RailsLite/bin/p01_basic_server.rb
@@ -1,1 +1,21 @@
 require 'rack'
+
+# Rack::Server.start(
+#   app: Proc.new do |env|
+#     ['200', {'Content-Type' => 'text/html'}, ['hello world']]
+#   end
+#   Port: 3000
+# )
+
+app = Proc.new do |env|
+    req = Rack::Request.new(env)
+    res = Rack::Response.new
+    res['Content-Type'] = 'text/html'
+    res.write("Hello world!")
+    res.finish
+  end
+
+Rack::Server.start({
+    app: app,
+    Port: 3000
+})
